7f06bf4ee55620a70137ebdc234ba83f725e2e17
Gabriel Rodriguez Diaz <gabriel_rodriguez_diaz@hotmail.com>
2016-08-21 04:39:19 -0500

    API movil informacion para el emprendedor

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
     {
 
         $user = Usuario::where('email',$email)->first();
-
+        /*
          $user->proyectos->each(function ($item, $key) {
                //$item->usuario->convocatorias_evaluadores;
                 $item->proyectos_integrantes;
@@ -28,15 +28,32 @@ class AuthController extends Controller
                        $item;
                });
         });
+        */
+
+        $data = ['id' => $user->id, 'response' => true,  'nombre' => $user->nombre, 'proyectos' => null];
+        for($i = 0; $i < count($user->proyectos); $i++){
+
+          // dd($user->proyectos[0]->proyectos_evaluadores[0]->convocatoria_evaluador->usuario);
+            $data['proyectos'][] = ['nombre' => $user->proyectos[$i]->nombre, 'descripcion' => $user->proyectos[$i]->descripcion,
+                'estado' => $user->proyectos[$i]->estado, 'convocatoria' => $user->proyectos[$i]->convocatoria->name];
+
+            for($x = 0; $x < count($user->proyectos[$i]->proyectos_integrantes); $x++){
+                $data['proyectos'][$i]['integrantes'] = array('nombre' => $user->proyectos[$i]->proyectos_integrantes[$x]->nombre,
+                    'email' => $user->proyectos[$i]->proyectos_integrantes[$x]->email, 'id' => $user->proyectos[$i]->proyectos_integrantes[$x]->id);
+            }
 
 
+            for($x = 0; $x < count($user->proyectos[$i]->proyectos_evaluadores); $x++){
+                $data['proyectos'][$i]['evaluadores'] = array('nombre' => $user->proyectos[$i]->proyectos_evaluadores[$x]->convocatoria_evaluador->usuario->nombre);
+            }
+        }
 
         if($user)
         {
             if($user->tipo == 1 && $user->password == $password)
             {
-                return response()->json(['id' => $user->id,'response' => true, 'nombre' => $user->nombre,
-                    'data' => $user],200);
+                return response()->json([
+                    'response' => $data],200);
             }
         }
 
